1dd176dab459b6612e75410bba69d8afad0ee4f4
Kittikorn Leelawanich <pkorn003@gmail.com>
2022-01-07 13:54:28 +0700

    Reset project

--- scripts/init.py
+++
@@ -1,5 +0,0 @@
-from brownie import KornGold, KornToken, accounts
-
-def main():
-    kornGold = KornGold.deploy({'from': accounts[0]})
-    kornToken = KornToken.deploy("KornToken", "KTN", kornGold.address, {'from': accounts[0]})
